fa70d31c6e64504968b3b28d31e073c8fe90792c
Joe Plonsker <joeplonsker@Joes-MacBook-Pro.local>
2016-12-21 16:57:31 -0500

    Add very first pretty board printing process

---
+++ sudoku.py
@@ -0,0 +1,6 @@
+class Sudoku:
+    def pretty_board(self, puzzle_string):
+
+        print puzzle_string
+
+Sudoku().pretty_board("1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--")
