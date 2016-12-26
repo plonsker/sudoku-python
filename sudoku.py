9a9a20678ee38aab3a1e7b2e29a1a0c2edf34778
joe <joeplonsker@gmail.com>
2016-12-26 17:29:40 -0500

    add column reference method

--- sudoku.py
+++ sudoku.py
@@ -6,11 +6,14 @@ def puzzle_dict(puzzle_str):
     print puzzle_master
 
 def row_ref(puzzle_str):
-    composite_list = [puzzle_str[x:x+9] for x in range(0, len(puzzle_str),9)]
-    print(composite_list)
+    puzzle_rows = [puzzle_str[x:x+9] for x in range(0, len(puzzle_str),9)]
+    puzzle_rows = [list(cell) for cell in puzzle_rows]
+    print(puzzle_rows)
 
 def column_ref(puzzle_str):
+    print map(list, zip(*([puzzle_str[x:x+9] for x in range(0, len(puzzle_str),9)])))
 
 practice_puzzle = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
 puzzle_dict(practice_puzzle)
 row_ref(practice_puzzle)
+column_ref(practice_puzzle)
