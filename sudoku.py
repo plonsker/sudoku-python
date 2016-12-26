7bfc6de5a775a2cf1e602dc448fbeea0db327f16
joe <joeplonsker@gmail.com>
2016-12-26 17:08:56 -0500

    add master dictionary and row reference methods

--- sudoku.py
+++ sudoku.py
@@ -1,21 +1,16 @@
 #no classes for now. this is an experiment with just methods.
 
-def board_rows(puzzle_string):
+def puzzle_dict(puzzle_str):
+    puzzle_str_arr = list(puzzle_str)
+    puzzle_master = dict(enumerate(puzzle_str_arr))
+    print puzzle_master
 
-        lol = lambda lst, sz: [lst[i:i+sz] for i in range(0, len(lst), sz)]
-        # http://stackoverflow.com/questions/4119070/how-to-divide-a-list-into-n-equal-parts-python
-        # for space in puzzle_string:
-            # if space != "-":
-                # # puzzle_string = map(int, puzzle_string)
-                # map(lambda puzzle_string: int(space) if space != "-", puzzle_string)
-                # 'lower' if x < 3 else 'higher' for x in lst
+def row_ref(puzzle_str):
+    composite_list = [puzzle_str[x:x+9] for x in range(0, len(puzzle_str),9)]
+    print(composite_list)
 
-        #Try to iterate through list and change anything that isn't a dash into an integer
-        int(space) if space != "-" else "-" for space in puzzle_string
+def column_ref(puzzle_str):
 
-        board_array = list(puzzle_string)
-        print lol(board_array, 9)
-
-
-
-board_rows("1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--")
+practice_puzzle = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
+puzzle_dict(practice_puzzle)
+row_ref(practice_puzzle)
