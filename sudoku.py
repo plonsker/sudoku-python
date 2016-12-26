c7f68dd4e82c22c8d21f2a9cc32e3fd5c833021c
joe <joeplonsker@gmail.com>
2016-12-26 18:09:13 -0500

    add first attempt at box creation

--- sudoku.py
+++ sudoku.py
@@ -11,9 +11,30 @@ def row_ref(puzzle_str):
     print(puzzle_rows)
 
 def column_ref(puzzle_str):
-    print map(list, zip(*([puzzle_str[x:x+9] for x in range(0, len(puzzle_str),9)])))
+    puzzle_columns = map(list, zip(*([puzzle_str[x:x+9] for x in range(0, len(puzzle_str),9)])))
+    print puzzle_columns
+
+def box_ref(puzzle_str):
+    puzzle_box_rows = [puzzle_str[x:x+3] for x in range(0, len(puzzle_str),3)]
+    puzzle_box_rows = [list(cell) for cell in puzzle_box_rows]
+    # print puzzle_box_rows
+
+    puzzle_boxes = []
+    i = 27
+
+    while i > 0:
+        for box_row in puzzle_box_rows:
+            puzzle_boxes.append(puzzle_box_rows[i-1])
+            puzzle_boxes.append(puzzle_box_rows[i-4])
+            puzzle_boxes.append(puzzle_box_rows[i-7])
+            i = i-1
+
+    # puzzle_boxes.reverse()
+    print puzzle_boxes
+
 
 practice_puzzle = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
-puzzle_dict(practice_puzzle)
-row_ref(practice_puzzle)
-column_ref(practice_puzzle)
+# puzzle_dict(practice_puzzle)
+# row_ref(practice_puzzle)
+# column_ref(practice_puzzle)
+box_ref(practice_puzzle)
