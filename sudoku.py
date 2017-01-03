7acc17bd3d22d3228578a75411568bdee7020854
Joe Plonsker <joeplonsker@ip-192-168-153-163.ec2.internal>
2017-01-03 12:04:33 -0500

    fix appending list to keep iterations low

--- sudoku.py
+++ sudoku.py
@@ -245,6 +245,7 @@ def solver1(puzzle_master,live_puzzle_dict,possible_solutions):
   i = 0
   # print live_puzzle_dict
   going = True
+  already_seen = []
 
   # while i < 81 and 9 > len(set(live_puzzle_dict[i][0])) and 9 > len(set(live_puzzle_dict[i][1])) and 9 > len(set(live_puzzle_dict[i][-1])):
   while going:
@@ -268,22 +269,18 @@ def solver1(puzzle_master,live_puzzle_dict,possible_solutions):
 
       puzzle_str_ref = ''.join(map(str, (new_puzzle_master.values())))
 
-      already_seen = []
-
+      #this is non-snesical. There will only be one puzzle in the list already_seen
+    #   already_seen = []
       if puzzle_str_ref not in already_seen:
               already_seen.append(puzzle_str_ref)
 
               row_set = map(set, row_ref(puzzle_str_ref))
-              print row_set
-              print "hello"
-              print set(map(len, row_set))
               column_set = map(set, column_ref(puzzle_str_ref))
               box_set = map(set, column_ref(puzzle_str_ref))
 
-              print "hey there"
-              print set(map(len, row_set))
-              print set(map(len, column_set))
-              print set(map(len, box_set))
+              print puzzle_str_ref
+              print "length of already_seen"
+              print len(already_seen)
 
 
 
@@ -300,11 +297,18 @@ def solver1(puzzle_master,live_puzzle_dict,possible_solutions):
               print i
 
 
-# practice_puzzle = "105802000090076405200400819019007306762083090000061050007600030430020501600308900"
-# practice_puzzle = "246857913189643275573291486418329567637485129952170048764532891321968754895710000"
+#puzzle one:
+practice_puzzle = "105802000090076405200400819019007306762083090000061050007600030430020501600308900"
+
+#nearly solved puzzle:
+# practice_puzzle = "246857913180000000000001486418329507637480000950170048764532891321968754895710000"
+
+#puzzle two:
 # practice_puzzle = "005030081902850060600004050007402830349760005008300490150087002090000600026049503"
-practice_puzzle = "639000024002904067070268090005603780701025400360040209510080940200419005043700610"
-# start_method(practice_puzzle)
+
+#easy kids puzzle
+# practice_puzzle = "639000024002904067070268090005603780701025400360040209510080940200419005043700610"
+
 puzzle_dict(practice_puzzle)
 row_ref(practice_puzzle)
 column_ref(practice_puzzle)
